de173e50bfa46124c8ea2b095077db0fde159ee4
esaharov <esaharov@osslabs.ru>
2018-09-26 14:39:06 +0400

    moya selsky

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import Step from './Step';
+
+const stepTitles = ['Personal information', 'Card information', 'Finish'];
+
+export default class App extends Component {
+	
+
+  render() {
+
+    return (
+    	<div>
+	    	фывфывфыв
+    	</div>
+	);
+  }
+}
